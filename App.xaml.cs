2e7d6af81194163d3deca531d791703c866f3827
Tataruca Mihai <tatarucamihai@yahoo.com>
2023-12-03 21:04:48 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,25 @@
-﻿namespace Tataruca_Mihai_Lab7;
+﻿using System;
+using Tataruca_Mihai_Lab7.Data;
+using System.IO;
+namespace Tataruca_Mihai_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
